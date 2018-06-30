c2c24fe4a621652df79007fb5f40a504beebf7b9
Erica Plofsky <ericaplofsky@gmail.com>
2018-06-30 11:09:00 -0400

    added concertlist component

--- client/src/components/ConcertList.js
+++ client/src/components/ConcertList.js
@@ -1,17 +1,22 @@
-// import React from 'react';
-//
-//
-//
-// const Concerts = ({ concerts }) => {
-//   const renderConcerts = concerts.map(concert =>
-//   <span key={concert.id}>{concert.band}</span>
-// )
-//
-//  return (
-//    <div>
-//    {renderConcerts}
-//    </div>
-//  )
-// }
-//
-// export default Concerts;
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const ConcertList = ({concerts}) => {
+
+  const renderConcerts = concerts.map(concert =>
+  <link key={concert.id}) to={`/concerts/${concert.id}`}>
+  <div>
+    <h2>{concert.band}</h2>
+  </div>
+  </link>
+);
+
+return (
+   <div>
+     {renderConcerts}
+   </div>
+ );
+};
+
+export default ConcertList;
