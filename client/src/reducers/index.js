ac2e31a1b045679e0650a96e0cef55aa5246d95d
Erica Plofsky <ericaplofsky@gmail.com>
2018-06-30 16:08:44 -0400

    deleted reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import concertsReducer from './concertsReducer';
-import concertFormData from './concertFormData';
 import commentsReducer from './commentsReducer';
 
 export default combineReducers({
-  concerts: concertFormData,
   concerts: concertsReducer,
   comments: commentsReducer
 });
