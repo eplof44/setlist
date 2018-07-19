4c571e1720edf418da954bf4b9e5e63132605f40
Erica Plofsky <ericaplofsky@gmail.com>
2018-07-19 19:21:27 -0400

    button persists

--- client/src/components/ConcertList.js
+++ client/src/components/ConcertList.js
@@ -1,12 +1,38 @@
 import { Link } from 'react-router-dom';
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { increaseLikes } from '../actions/concerts';
+import { bindActionCreators } from 'redux';
+
 import '../styles/Concerts.css'
 
-const ConcertList = (props) =>
+
+
+class ConcertList extends Component {
+
+
+  render() {
+    const { concert } = this.props;
+    return(
+
       <div>
-          <li><Link key={props.concert.id} to={`/concerts/${props.concert.id}`}>{props.concert.band} on {props.concert.date} at {props.concert.venue}  </Link></li>
+      <li><Link key={this.props.concert.id} to={`/concerts/${this.props.concert.id}`}>{this.props.concert.band} on {this.props.concert.date} at {this.props.concert.venue}  </Link></li>
+     <button onClick={() => this.props.increaseLikes(this.props.concert)}> 
+      <span> Like </span>
+      <div> {this.props.concert.likes} </div>
+      </button>
       </div>
+    )
+  }
+}
 
 
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    increaseLikes
+  }, dispatch);
+}
 
-export default ConcertList;
+
+export default connect(null,mapDispatchToProps)(ConcertList);
